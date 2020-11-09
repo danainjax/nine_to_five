40fce16ab8d90e0429f6443ac7833d352f93e2c0
danainjax <danainjax@gmail.com>
2020-11-09 12:45:41 -0500

    create route added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,27 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/jobs/new' do #loads new form
+    erb :new
+  end
+
   get "/" do
     erb :welcome
   end
 
   get "/jobs" do
-    erb :jobs
-    # Job.all.each do |job|
-    #   job.title
-    # end
+    @jobs = Job.all
+    erb :index
+  end
+
+  get '/jobs/:id' do  #loads show page
+    @job = Job.find_by_id(params[:id])
+    erb :show
+  end
+
+  post '/jobs' do  #creates a job
+    @job = Job.create(params)
+    redirect to "/jobs/#{@job.id}"
   end
 
 end
