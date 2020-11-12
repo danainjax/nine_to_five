681ee875f2fc1d3d3d12474cafa5fcfad2a53c5d
danainjax <danainjax@gmail.com>
2020-11-12 13:46:42 -0500

    added require_login helper method and refactored app controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,68 +1,57 @@
 class JobsController < ApplicationController
 
     get '/jobs/new' do #loads new form
-        if logged_in?
-            erb :new
-        else 
-            erb :'users/login'
-        end
-      end
+        require_login
+        erb :new
+    end
     
     
-      get "/jobs" do
+    get "/jobs" do
         @jobs = Job.all
-        if logged_in?
-            erb :index
-        else
-            erb :'users/login'
-        end
-      end
+        require_login
+        erb :index
+    end
     
-      get '/jobs/:id' do  #loads show page
-        if logged_in?
+    get '/jobs/:id' do  #loads show page
+        require_login
         # binding.pry
-            @job = Job.find_by_id(params[:id])
-            erb :show
-        else
-            erb :'users/login'
-        end
-      end
+        @job = Job.find_by_id(params[:id])
+        erb :show
+    end
+        
+      
     
-      post '/jobs' do  #creates a job
+    post '/jobs' do  #creates a job
         # @job = Job.create(params)
         # redirect to "/jobs/#{@job.id}"
         job = current_user.jobs.build(params)
         job.save
         redirect to "/jobs/#{job.id}"
-      end
+    end
     
-      get '/jobs/:id/edit' do #loads edit form
-        if logged_in?
+    get '/jobs/:id/edit' do #loads edit form
+        require_login
             @job = Job.find_by_id(params[:id])
             erb :edit
-        else
-            erb "/users/login"
-        end
-      end
+    end
     
     
-      patch '/jobs/:id' do  #updates a job
+    
+    patch '/jobs/:id' do  #updates a job
         @job = Job.find_by_id(params[:id])
         @job.title = params[:title]
         @job.company = params[:company]
         @job.description = params[:description]
         @job.save
         redirect to "/jobs/#{@job.id}"
-      end
+    end
     
-      delete '/jobs/:id' do #destroy action
-        if logged_in?
+    delete '/jobs/:id' do #destroy action
+        require_login
             @job = Job.find_by_id(params[:id])
             @job.delete
             redirect to '/jobs'
-        else
-            erb :'/users/login'
-        end
-      end
+    end
+      
     
 end
