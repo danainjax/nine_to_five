24f6c4ba64e81c4fb1e9c2fc5e82c5af7538ae6e
danainjax <danainjax@gmail.com>
2020-11-12 10:11:38 -0500

    added validation to ensure cannot signup if account with username already exists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,9 @@ class UsersController < ApplicationController
             if user.username.empty? || user.password.empty?
                 @error = "Username and password cannot be blank"
                 erb :'users/signup'
+            elsif User.find_by(username: user.username)
+                @error = "Account with that username already exists"
+                erb :'users/signup'
             else
                 user.save
                 session[:user_id] = user.id
