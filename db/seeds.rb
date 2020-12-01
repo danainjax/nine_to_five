53f3fdbce1590c864b5c98006e3d1f587f682125
danainjax <danainjax@gmail.com>
2020-11-30 20:05:53 -0500

    fixed link with proper a tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,3 +37,80 @@ You will work with key stakeholders across the business to create, test and impr
 Ø Understanding of the formal software development lifecycle (SDLC)
 
 Ø Understanding of test-driven development")
+
+Job.create(title: "Support Technologies Junior Software Developer", company: "Apple", location: "Austin, TX", description: "The people here at Apple don’t just craft products - they build the kind of wonder that’s revolutionized entire industries. It’s the diversity of those people and their ideas that inspires the innovation that runs through everything we do, from amazing technology to industry-leading environmental efforts. Join Apple, and help us leave the world better than we found it.
+
+
+    The AppleCare Support Technologies team is seeking energetic and motivated junior software developers with a solid understanding of either web or native app development. As a key member of our software team, you will write, maintain, and debug software that enhances the support of our Apple Support teams. At Apple, phenomenal ideas have a way of becoming great products, services, and customer experiences. Bring passion and dedication to your job and there's no telling what you could accomplish.
+    
+    
+    This role can be onsite in Austin or be At-Home.As the Support Technologies Development team, we are tasked to collaborate, design, build, maintain and debug vital parts of the Apple support infrastructure, supporting up to 50,000 users in their daily activities.
+
+
+    Responsibilities include:
+    
+    
+    Work both independently and as part of the wider development team in creating both native and full-stack web applications
+    
+    
+    Focus on security and data privacy at all stages of development
+    
+    
+    Develop prototypes and proof-of-concept applications in addition to larger scale software
+    
+    
+    Be curious, excited to proactively learn new technologies, and teach what you’ve learned to your colleagues", key_qualifications: "Experience creating an entire app on your own — as part of your current role, through classwork, or a side project.
+
+    Native iOS/macOS app development using Swift or Objective-C
+    
+    Web application development using languages such as Ruby, Python, PHP, or JavaScript with frameworks such as Rails, Django, Laravel, or Express
+    
+    Experience using some kind of data storage technology such as Core Data, Postgres, MySQL, Redis, or S3
+    
+    Experience using web services (REST or GraphQL APIs) with JSON, and a high-level understanding of how HTTP works
+    
+    Experience deploying to cloud platforms such as Heroku or Amazon Web Services
+    
+    Experience with shell scripting using Bash or ZSH
+    
+    Familiarity with Git and using GitHub to write great pull requests
+    
+    Passion for great design — both technical design and visual design
+    
+    Ability to work effectively on a distributed team using clear written communication
+    
+    Ability to build deep customer relationships, be results-oriented, detail focused, and well organized
+    
+    Ability to wear different hats; possession of do-whatever-it-takes ability and attitude
+       
+    Bachelor’s degree in Computer Science, or equivalent experience", application_url: "https://jobs.apple.com/en-us/details/200191770/support-technologies-junior-software-developer")
+
+    Job.create(title: "Software Developer", company: "Swoon", location: "New Orleans, Louisiana, United States", description: "Located in the Greater New Orleans Area and is one of the fastest-growing tech companies in Louisiana. They develop custom web-based business software with a focus on legacy system conversions. Their primary development platforms are C# .NET for web services and Ionic/Cordova for mobile apps.Responsibilities 
+
+    Application Development, primarily ASP.NET (C#.NET) , JavaScript,
+    Database Design and Maintenance
+    Perform work within allotted hourly budgets and within specific timeframes 
+    Assist with Requirements analysis
+    Assist with the development of scope documents and budgets for new work 
+    Assist with Client communication ", key_qualifications: "3 Years web development experience
+    Elicitation of requirements from non-technical business users
+    Conceptually design systems based on business requirements
+    Perform technical lead role through the SDLC
+    Highly proficient in database design and SQL
+    Highly proficient in a server-side language such as C#, Java, Ruby, Python, or PHP
+    Highly proficient with Javascript
+    Experience with CSS/HTML
+    Ability to adapt to rapidly changing environments
+    Self-motivated with a positive attitude
+    Willingness to drive projects to completion in a fast-moving environment 
+    
+    Nice-to-Have
+    
+    Experience with Kendo UI
+    Experience with Legacy system conversions 
+    BS/BA degree
+    Azure experience
+    Linux experience 
+    Mobile development experience
+    C# Expertise
+    Microsoft Visual Studio", application_url: "https://swoonstaffing.com/work-at-swoon/")
